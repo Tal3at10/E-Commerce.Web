a67ce11614804f634138a0e2b98966a61c1ceab1
Tal3at10 <mt7993047@gmail.com>
2025-05-07 19:57:35 +0300

    Add project files.

---
+++ Core/Service/MappingProfiles/ProductProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using DomainLayer.Models;
+using Shared.DataTransferObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.MappingProfiles
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<Product, ProductDto>()
+                .ForMember(dist => dist.BrandName, Options => Options.MapFrom(src => src.ProductBrand.Name))
+                .ForMember(dist => dist.TypeName, Options => Options.MapFrom(src => src.ProductType.Name))
+                .ForMember(dist => dist.PictureUrl, Options => Options.MapFrom<PictureUrlResolver>());
+
+            CreateMap<ProductType, TypeDto>();
+            CreateMap<ProductBrand, BrandDto>();
+        }
+    }
+}
+ 
